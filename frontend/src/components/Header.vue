8125a6a6b11d0a5a4043594db51f9bce739cd7d9
MingoMango17 <junry.mingo17@gmail.com>
2024-02-06 21:32:53 +0800

    Add tailwind styles to Header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,26 +1,30 @@
 <template>
-    <header class="text-midnight py-4 px-6 flex justify-between items-center">
-        <!-- Logo -->
-        <div class="logo">
-            Your Logo Here
-        </div>
-        
-        <!-- Navigation -->
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
+    <div class="mx-auto w-5/6">
+        <header class="container text-midnight py-4 px-6 flex justify-between items-center">
+            <!-- Logo -->
+            <div class="logo">
+                Your Logo Here
+            </div>
+            <h1 class="m-4 font-bold">Artefy</h1>
+            
+            <!-- Navigation -->
+            <nav class="mx-auto flex justify-between items-center">
+                <ul class="flex space-x-4">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
 
-        <!-- Login and Sign Up Buttons -->
-        <div class="buttons">
-            <button class="login-btn">Log In</button>
-            <button class="signup-btn">Sign Up</button>
-        </div>
-    </header>
+            <!-- Login and Sign Up Buttons -->
+            <div class="buttons flex space-x-2">
+                <button class="login-btn">Log In</button>
+                <button class="signup-btn">Sign Up</button>
+            </div>
+        </header>
+        <hr/>
+    </div>
 </template>
 
 <script setup>
