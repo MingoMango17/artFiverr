00cd4ae80ddf55e110b6ac0450b7aed6c4b54205
Zian Rinzler Valles <83147228+Miraku17@users.noreply.github.com>
2024-02-13 02:27:22 +0800

    Added features in the homepage

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,119 +1,128 @@
 <template>
-    <div class="mx-auto bg-dirty-white flex space-x-2">
-        <div class="w-2/3">
-            <h1 class="mt-20 mr-10 mb-5 font-bold text-5xl text-army-green">
-                Unleash your imagination; commission the finest artistry tailored to your vision.
-            </h1>
-            <h2 class="ml-20 ml-0 text-base">
-                "Artefy: Your artistic journey, tailored for you."
-            </h2>
-            <div class="flex space-x-3 mt-10 mb-24">
-                <div>
-                    <h2 class="mr-16 font-bold text-4xl text-army-green">
-                        200
-                    </h2>
-                    <p class="text-army-green">
-                        Artists
-                    </p>
-                </div>
-                <div>
-                    <h2 class="mr-16 font-bold text-4xl text-army-green">
-                        300
-                    </h2>
-                    <p class="text-army-green">
-                        Artworks
-                    </p>
-                </div>
-                <div>
-                    <h2 class="font-bold text-4xl text-army-green">
-                        500
-                    </h2>
-                    <p>
-                        Orders Received
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="">
-            <img src="../assets/artist.svg" alt="Artist"
-                class="object-center"/>
-        </div>
-    </div>
-    <div class="bg-army-green rounded-lg p-4 text-white">
-        <h1>
-            Need Something Done?
+    <div class="mx-auto bg-brownish flex space-x-2 " >
+      <div class="w-2/3 font-dm-sans ">
+        <h1
+          class="mt-20 mr-10 mb-5 font-semibold text-5xl text-army-green "
+          v-motion-pop-visible-once
+        >
+          Unleash your imagination; commission the finest artistry tailored to
+          your vision.
         </h1>
-        <div class="flex space-x-4">
-            <div>
-                <h2 class="">
-                    Browse an Artwork
-                </h2>
-                <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec aliquam metus. Etiam mauris ligula, congue nec neque nec, elementum maximus nisl.
-                </p>
-            </div>
-            <div>
-                <h2 class="">
-                    Browse an Artwork
-                </h2>
-                <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec aliquam metus. Etiam mauris ligula, congue nec neque nec, elementum maximus nisl.
-                </p>
-            </div>
-            <div>
-                <h2 class="">
-                    Browse an Artwork
-                </h2>
-                <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec aliquam metus. Etiam mauris ligula, congue nec neque nec, elementum maximus nisl.
-                </p>
-            </div>
-            <div>
-                <h2 class="">
-                    Browse an Artwork
-                </h2>
-                <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec aliquam metus. Etiam mauris ligula, congue nec neque nec, elementum maximus nisl.
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="bg-white p-10">
-        <h2>
-            Featured Arts
+        <h2 class="text-base font-dm-sans font-bold">
+          "Artefy: Your artistic journey, tailored for you."
         </h2>
-        <div class="flex m-10">
-            <div v-for="art in featArts" class="w-1/5">
-                <h2>{{ art.title }}</h2>
-                <p>{{ art.artist }}</p>
-            </div>
+        <div class="flex space-x-3 mt-10 mb-24 font-dm-sans">
+          <div>
+            <h2 class="mr-16 text-4xl text-army-green">200</h2>
+            <p class="text-gray-700">Artists</p>
+          </div>
+          <div>
+            <h2 class="mr-16  text-4xl text-army-green">300</h2>
+            <p class="text-gray-700">Artworks</p>
+          </div>
+          <div>
+            <h2 class=" text-4xl text-army-green">500</h2>
+            <p class="text-gray-700">Orders Received</p>
+          </div>
         </div>
+      </div>
+      <div class="">
+        <img src="../assets/artist.svg" alt="Artist" class="object-center" />
+      </div>
     </div>
-</template>
-
-<script setup>
-    import { ref } from 'vue';
-
-    const featArts = ref([
-        {
-            title: 'Wallowing Breeze',
-            artist: 'Elena Monroe',
-        },
-        {
-            title: 'Wallowing Breeze',
-            artist: 'Nhicole Munasque',
-        },
-        {
-            title: 'Wallowing Breeze',
-            artist: 'Nhicole Munasque',
-        },
-        {
-            title: 'Wallowing Breeze',
-            artist: 'Nhicole Munasque',
-        },
-        {
-            title: 'Wallowing Breeze',
-            artist: 'Nhicole Munasque',
-        },
-    ])
-</script>
+    <div class="bg-army-green rounded-lg p-8 text-white font-dm-sans">
+      <h1 class="font-bold text-2xl mb-10">Need Something Done?</h1>
+      <div class="flex space-x-4">
+        <div class="mb-4">
+          <img class="w-15 h-auto" src="../assets/painting.svg" alt="Painting" />
+          <h2 class="mt-2 mb-1 text-lg font-extralight ">Browse an Artwork</h2>
+          <p class="text-sm mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            nec aliquam metus. Etiam mauris ligula, congue nec neque nec,
+            elementum maximus nisl.
+          </p>
+        </div>
+        <div>
+          <img class="w-15 h-auto" src="../assets/icon-artist.svg" alt="Artist" />
+          <h2 class="mt-2 mb-1 text-lg font-extralight">Choose Artist</h2>
+          <p class="text-sm mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            nec aliquam metus. Etiam mauris ligula, congue nec neque nec,
+            elementum maximus nisl.
+          </p>
+        </div>
+        <div>
+          <img class="w-15 h-auto" src="../assets/message.svg" alt="Message" />
+  
+          <h2 class="mt-2 mb-1 text-lg font-extralight">Message them</h2>
+          <p class="text-sm mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            nec aliquam metus. Etiam mauris ligula, congue nec neque nec,
+            elementum maximus nisl.
+          </p>
+        </div>
+        <div>
+          <img class="w-15 h-auto" src="../assets/payment.svg" alt="Payment" />
+  
+          <h2 class="mt-2 mb-1 text-lg font-extralight">Pay safely</h2>
+          <p class="text-sm mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            nec aliquam metus. Etiam mauris ligula, congue nec neque nec,
+            elementum maximus nisl.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white p-10 my-10">
+      <h2>Featured Arts</h2>
+      <div class="flex m-10">
+        <div v-for="art in featArts" class="w-1/5">
+          <h2>{{ art.title }}</h2>
+          <p>{{ art.artist }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  
+    .animate-fade-in {
+      animation: fadeIn 1s ease-in-out;
+    }
+  </style>
+  
+  <script setup>
+  import { ref } from "vue";
+  
+  const featArts = ref([
+    {
+      title: "Wallowing Breeze",
+      artist: "Elena Monroe",
+    },
+    {
+      title: "Wallowing Breeze",
+      artist: "Nhicole Munasque",
+    },
+    {
+      title: "Wallowing Breeze",
+      artist: "Nhicole Munasque",
+    },
+    {
+      title: "Wallowing Breeze",
+      artist: "Nhicole Munasque",
+    },
+    {
+      title: "Wallowing Breeze",
+      artist: "Nhicole Munasque",
+    },
+  ]);
+  </script>
+  
